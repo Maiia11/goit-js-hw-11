8a5619578c255f49bc884a6b94ac4abf16c47f8f
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-10 14:26:55 +0300

    HTTP -запити

--- src/main.js
+++ src/main.js
@@ -1,4 +1,56 @@
-// Описаний у документації
+
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const apiKey = "43226276-a07a0c17e428cfffb021b9b05";
+
+const form = document.querySelector(".picture-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = event.target.elements.choose.value;
+
+    if (!input.length) {
+        iziToast.info({
+            message: "Sorry, there are no images matching your search query. Please try again!"
+
+        })
+        return
+    }
+
+    const params = new URLSearchParams({
+    key: apiKey,
+    q: input,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true
+    
+    })
+    
+    return fetch(`https://pixabay.com/api/?${params}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    
+}
+
+
+
+
+
+
+
+
